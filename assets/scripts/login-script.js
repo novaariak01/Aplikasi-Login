72eb766a984e4a279dcd0cdf05acafd3161eacbd
Aria Kirana <72263904+novaariak01@users.noreply.github.com>
2022-04-14 14:13:30 +0700

    Tambah Komentar
    Penambahan komentar pada tiap-tiap fungsi

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,32 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/* Comment: Membuat variable untuk semua elemen view. */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/* Comment: Membuat variable untuk menyimpan informasi email dan password. */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/* Comment: Menambahlan aksi klik pada button*/
 loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
+    // Comment: Mendapatkan nilai dari input email dan password pengguna.
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+  
+    /* Comment: Memastikan nilai input sesuai dengan nilai yang tersimpan*/
+
+    if (email == expectedEmail && password == expectedPassword) {
+     
+    /* Comment: Jika nilai sesuai maka akan menuju ke halaman home. */ 
+      goToHome();
+    } else {
+
+    /* Comment: Jika tidak akan muncul informasi input salah. */
+      showPopUp();
+    }
 });
